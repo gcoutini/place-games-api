c7be7454ec2e81862eb13a95cb21a8bf84bd2bf9
gcoutini <61057977+gcoutini@users.noreply.github.com>
2021-01-25 03:08:08 -0300

    adds consulting to existing customer

--- resources/routes/customer.js
+++ resources/routes/customer.js
@@ -5,10 +5,21 @@ const Customer = require("../models/Customer");
 
 router.post("/register_client", async (req, res) => {
   const { name, cpf, birth_date, email, tel, cel } = req.body;
+  const customerExist = await Customer.findOne({ cpf });
+  if(customerExist) {
+    return res.status(400).json({ message: "Usuário já cadastrado."});
+  }
   let customer = new Customer({ name, cpf, birth_date, email, tel, cel })
-
   await customer.save();
-  res.send(customer);
+  res.status(201).json(customer);
 })
+
+router.post("/verify_client", async (req, res) => {
+  const { name, cpf } = req.body;
+  const customer = await Customer.findOne({ name });
+  console.log(customer);
+  return !customer? res.send(404) : res.status(200).json(customer);
+
+});
 
 module.exports = router;
