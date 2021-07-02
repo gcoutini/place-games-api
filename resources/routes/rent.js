2cbdd4be4f715a587c62aa3eed83591b8ef5cf10
Gabriel Coutini <gabriel.coutini@globalhitss.com.br>
2021-07-01 22:29:13 -0300

    adds paidFor field and set available to false after rent

--- resources/routes/rent.js
+++ resources/routes/rent.js
@@ -3,16 +3,21 @@ const router = express.Router();
 
 const Rent = require("../models/Rent");
 const Customer = require("../models/Customer");
+const Game = require("../models/Game");
 
 router.post("/new_rent", async (req, res) => {
-  const { client, cpf, title, rent_date, return_date, rental_days, price } = req.body;
-  let rent = new Rent({ client, cpf, title, rent_date, return_date, rental_days, price })
+  const { client, cpf, title, rent_date, return_date, rental_days, price, paidFor } = req.body;
+  let rent = new Rent({ client, cpf, title, rent_date, return_date, rental_days, price, paidFor })
   const rent_id = rent._id;
   await rent.save();
   await Customer.updateOne(
     { cpf: cpf },
     { $push: { rentals: rent_id }}
   );
+  await Game.updateOne(
+    { title: title},
+    { available: false}
+  )
   return res.status(201).json(rent);
 })
 
