41de25e4e1daa56f8aca34fab0d82fe336631f3b
Gabriel Coutini <gabriel.coutini@globalhitss.com.br>
2021-07-01 22:28:34 -0300

    updates mongoose connection string

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-    mongoose.connect('mongodb://localhost:27017/place-games')
+    mongoose.connect('mongodb://localhost:27017/place-games', { useNewUrlParser: true});
     let db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
 
